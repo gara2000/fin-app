fa7a486669f6b8c3665b2a48c5c12ce6fb425ed9
gara <firasgara@hotmail.fr>
2024-05-20 19:08:43 +0200

    changed main dir

--- src/data.ts
+++ src/data.ts
@@ -2,11 +2,6 @@ import { Item } from "./app/shared/models/item";
 
 export const assets: Item[] = [
     {
-        name: "Scolarship",
-        type: "asset",
-        currValue: 1200
-    },
-    {
         name: "Last Month Net",
         type: "asset",
         currValue: 100
@@ -14,11 +9,6 @@ export const assets: Item[] = [
 ]
 
 export const liabilities: Item[] = [
-    {
-        name: "Rent",
-        type: "liability",
-        currValue: 450
-    },
     {
         name: "Savings",
         type: "liability",
